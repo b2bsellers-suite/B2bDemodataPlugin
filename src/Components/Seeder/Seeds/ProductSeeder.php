fd625183d8bb9a62a24f7f6323ca53b9a003e5b0
Ben <b.letzel@b2b-sellers.com>
2025-01-27 11:55:02 +0100

    BCS-2182 added pruduct with purchase prices to dataseeder

--- src/Components/Seeder/Seeds/ProductSeeder.php
+++ src/Components/Seeder/Seeds/ProductSeeder.php
@@ -18,6 +18,11 @@ class ProductSeeder
 {
 	private Context $context;
 
+    const PRICE_FIELDS = [
+        'price',
+        'purchasePrices'
+    ];
+
 	public function __construct(
         private ContainerInterface $container,
         private Connection $connection)
@@ -130,14 +135,20 @@ class ProductSeeder
 			return $productJson;
 		}
 
-		foreach ($productJson['price'] as $key => $price) {
-			if (!isset($price['currencyCode'])) {
-				continue;
-			}
+        foreach (self::PRICE_FIELDS as $field) {
+            if(!key_exists($field, $productJson)){
+                continue;
+            }
 
-			$productJson['price'][$key]["currencyId"] = $this->getCurrentCurrencyId($price['currencyCode']);
-			unset($productJson['price'][$key]["currencyCode"]);
-		}
+            foreach ($productJson[$field] as $key => $price) {
+                if (!isset($price['currencyCode'])) {
+                    continue;
+                }
+
+                $productJson[$field][$key]["currencyId"] = $this->getCurrentCurrencyId($price['currencyCode']);
+                unset($productJson[$field][$key]["currencyCode"]);
+            }
+        }
 
 		return $productJson;
 	}
