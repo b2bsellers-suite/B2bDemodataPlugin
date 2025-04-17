9eb64af1c66a153fb619e5b3434a4863e4a6ff8d
Jan-Erik Spreng <j.spreng@b2b-sellers.com>
2025-04-17 15:33:40 +0200

    BCS-2392: Optimize and clean up command to generate customers

--- src/Command/CustomerGenerateCommand.php
+++ src/Command/CustomerGenerateCommand.php
@@ -2,9 +2,12 @@
 
 namespace B2bDemodata\Command;
 
+use B2bSellersCore\Components\SalesRepresentative\SalesRepresentativeCustomerCollection;
+use B2bSellersCore\Components\SalesRepresentative\SalesRepresentativeCustomerEntity;
 use Faker\Factory;
 use Faker\Generator;
 use Maltyxx\ImagesGenerator\ImagesGeneratorProvider;
+use Shopware\Core\Checkout\Customer\CustomerCollection;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Customer\SalesChannel\AbstractRegisterRoute;
 use Shopware\Core\Defaults;
@@ -15,9 +18,13 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Demodata\Faker\Commerce;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\SalesChannel\Aggregate\SalesChannelDomain\SalesChannelDomainCollection;
+use Shopware\Core\System\SalesChannel\Aggregate\SalesChannelDomain\SalesChannelDomainEntity;
 use Shopware\Core\System\SalesChannel\Context\AbstractSalesChannelContextFactory;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -36,6 +43,9 @@ class CustomerGenerateCommand extends Command
         private readonly EntityRepository                   $employeeRepository,
         private readonly EntityRepository                   $employeeCustomerRepository,
         private readonly EntityRepository                   $salesRepresentativeCustomerRepository,
+        private readonly EntityRepository                   $salesChannelDomainRepository,
+        private readonly EntityRepository                   $countryRepository,
+        private readonly EntityRepository                   $salutationRepository,
     )
     {
         parent::__construct();
@@ -44,16 +54,6 @@ class CustomerGenerateCommand extends Command
 
     protected function configure(): void
     {
-        $this->addArgument('storefrontUrl', InputArgument::REQUIRED, 'Storefront URL');
-        // TODO: Get random country
-        $this->addArgument('countryId', InputArgument::REQUIRED, 'Country UUID');
-        // TODO: Get id from url
-        $this->addArgument('salesChannelId', InputArgument::REQUIRED, 'Sales channel UUID');
-        // TODO: Get random salutation
-        $this->addArgument('salutationId', InputArgument::REQUIRED, 'Salutation UUID');
-        // TODO: Get random sales rep
-        $this->addArgument('salesRepId', InputArgument::REQUIRED, 'Sales Rep UUID');
-
         $this->addOption('customer-amount', null, InputOption::VALUE_OPTIONAL, 'How many customers should be generated', 500);
         $this->addOption('employee-amount', null, InputOption::VALUE_OPTIONAL, 'How many employees should be generated', 50);
     }
@@ -62,28 +62,75 @@ class CustomerGenerateCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $context = $this->salesChannelContextFactory->create(Uuid::randomHex(), $input->getArgument('salesChannelId'));
-        $customerIds = [];
+        $defaultContext = Context::createDefaultContext();
+        $domains = $this->salesChannelDomainRepository->search(new Criteria(), $defaultContext)->getEntities();
+        $salesReps = $this->customerRepository->search((new Criteria())->addFilter(new EqualsFilter('customFields.b2b_sales_representative', true)), $defaultContext)->getEntities();
+        $countries = $this->countryRepository->searchIds(new Criteria(), $defaultContext)->getIds();
+        $salutations = $this->salutationRepository->searchIds(new Criteria(), $defaultContext)->getIds();
+
+        $storefrontUrl = $this->getStorefrontUrl($io, $domains);
+        $salesChannelId = $this->getSalesChannelId($storefrontUrl, $domains);
+        $salesRep = $this->getSalesRep($io, $salesReps);
+        $salesRepId = $this->getSalesRepId($salesRep, $salesReps);
+        $context = $this->salesChannelContextFactory->create(Uuid::randomHex(), $salesChannelId);
 
         $io->writeln('Creating customers');
-
         $progress = $io->createProgressBar($input->getOption('customer-amount'));
+        $customerIds = $this->createCustomers(
+            $input->getOption('customer-amount'),
+            $storefrontUrl,
+            $countries,
+            $progress,
+            $context
+        );
+
+        $io->writeln('Updating custom fields and setting sales rep');
+        $this->updateCustomerCustomFields($customerIds, $context->getContext());
+        $this->addSalesRepCustomer($salesRepId, $customerIds, $context->getContext());
+
+        $io->writeln('Creating employees');
+
+        $progress = $io->createProgressBar($input->getOption('employee-amount'));
+        $employees = $this->createEmployees(
+            $input->getOption('employee-amount'),
+            $salutations,
+            $progress,
+            $context
+        );
+
+        $io->writeln('Mapping employees to customers');
+        $this->mapEmployeesToCustomers($customerIds, $employees, $context);
+
+        $io->success('Finished');
+
+        return 0;
+    }
+
+    private function createCustomers(
+        int $limit,
+        string $storefrontUrl,
+        array $countries,
+        ProgressBar $progress,
+        SalesChannelContext $context
+    ): array
+    {
+        $customerIds = [];
+
         $progress->start();
 
-        for ($i = 0; $i <= $input->getOption('customer-amount'); $i++) {
+        for ($i = 0; $i <= $limit; $i++) {
             try {
                 $customerResponse = $this->registerRoute->register(
                     $this->createCustomerDataBag(
-                        $input->getArgument('storefrontUrl'),
-                        $input->getArgument('countryId'
-                        )
+                        $storefrontUrl,
+                        $countries[random_int(0, count($countries) - 1)],
                     ),
                     $context
                 );
 
                 $customerIds[] = $customerResponse->getCustomer()->getId();
             } catch (\Throwable $e) {
-                $io->warning($e->getMessage());
+                // Ignore
             }
 
             if ($i % 10 === 0) {
@@ -93,28 +140,28 @@ class CustomerGenerateCommand extends Command
 
         $progress->finish();
 
-        $io->writeln('Updating custom fields and setting sales rep');
+        return $customerIds;
+    }
 
-        $this->updateCustomerCustomFields($customerIds, $context->getContext());
-        $this->addSalesRepCustomer($input->getArgument('salesRepId'), $customerIds, $context->getContext());
-
-        $io->writeln('Creating employees');
-
-        $progress = $io->createProgressBar($input->getOption('employee-amount'));
+    private function createEmployees(int $limit, array $salutations, ProgressBar $progress, SalesChannelContext $context): array
+    {
         $progress->start();
 
         $employees = [];
 
-        for ($i = 0; $i <= $input->getOption('employee-amount'); $i++) {
-            $employees[] = $this->prepareEmployee($input->getArgument('salutationId'));
+        for ($i = 0; $i <= $limit; $i++) {
+            $employees[] = $this->prepareEmployee($salutations[random_int(0, count($salutations) - 1)]);
             $progress->advance();
         }
 
         $this->employeeRepository->upsert($employees, $context->getContext());
         $progress->finish();
 
-        $io->writeln('Mapping employees to customers');
+        return $employees;
+    }
 
+    private function mapEmployeesToCustomers(array $customerIds, array $employees, SalesChannelContext $context): void
+    {
         $mapping = [];
 
         foreach ($customerIds as $customerId) {
@@ -144,10 +191,40 @@ class CustomerGenerateCommand extends Command
         }
 
         $this->employeeCustomerRepository->upsert($mapping, $context->getContext());
+    }
 
-        $io->success('Finished');
+    private function getStorefrontUrl(SymfonyStyle $io, SalesChannelDomainCollection $domains): string
+    {
+        return $io->choice(
+            'Select storefront URL',
+            array_values($domains->map(function (SalesChannelDomainEntity $domain) {
+                return $domain->getUrl();
+            }))
+        );
+    }
 
-        return 0;
+    private function getSalesRep(SymfonyStyle $io, CustomerCollection $salesReps): string
+    {
+        return $io->choice(
+            'Select sales rep',
+            array_values($salesReps->map(function (CustomerEntity $salesRep) {
+                return $salesRep->getEmail();
+            }))
+        );
+    }
+
+    private function getSalesRepId(string $email, CustomerCollection $salesReps): string
+    {
+        return $salesReps->filter(function (CustomerEntity $salesRep) use ($email) {
+            return $salesRep->getEmail() === $email;
+        })->first()->getId();
+    }
+
+    private function getSalesChannelId(string $url, SalesChannelDomainCollection $domains): string
+    {
+        return $domains->filter(function (SalesChannelDomainEntity $domain) use ($url) {
+            return $domain->getUrl() === $url;
+        })->first()->getSalesChannelId();
     }
 
     private function addSalesRepCustomer(string $salesRepId, array $customerIds, Context $context): void
