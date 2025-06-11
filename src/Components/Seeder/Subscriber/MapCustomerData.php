aca6213295c4b07cf932cb2e56e4a713aacd7a2b
Benjamin Letzel <b.letzel@b2b-sellers.com>
2025-06-11 10:13:02 +0200

    BCS-2543 [-] Fixed Demodata Plugin Seeder to get exact customernumber from json file + added subscriber to retrieve customernumber from databag + fixed corrupted data in semiconductor json file

---
+++ src/Components/Seeder/Subscriber/MapCustomerData.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace B2bDemodata\Components\Seeder\Subscriber;
+
+use Shopware\Core\Checkout\Customer\CustomerEvents;
+use Shopware\Core\Framework\Event\DataMappingEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class MapCustomerData implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            CustomerEvents::MAPPING_REGISTER_CUSTOMER => 'mapCustomerData',
+        ];
+    }
+
+    public function mapCustomerData(
+        DataMappingEvent $event
+    ): void
+    {
+        $dataBag = $event->getInput();
+        $customer = $event->getOutput();
+
+        $customer['customerNumber'] = $dataBag->get('customerNumber');
+        dump($customer['customerNumber']);
+
+        $event->setOutput($customer);
+    }
+
+}
